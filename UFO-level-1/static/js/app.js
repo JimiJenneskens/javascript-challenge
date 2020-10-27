f48e7f81d2f00af18b890a97d1a88ad21e2c7a35
Jimi <jimijenneskens@gmail.com>
2020-10-27 13:23:12 -0600

    filteradded

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -19,4 +19,26 @@ function buildTable(data){
     });
 };
 
+// Create function to Filter date
+function handleClick() {
+    
+    // Get the date from the filter
+    const date = d3.select("#datetime").property("value");
+
+    // Create new variable of the original data
+    var filteredData = tableData; 
+
+    if (date) {
+        // Create new table with filtered data
+        var filteredData = filteredData.filter(info => info.datetime === date);
+    };
+
+    buildTable(filteredData)
+
+};
+
+
+d3.selectAll("#filter-btn").on('click', handleClick);
+
+
 buildTable(tableData);
