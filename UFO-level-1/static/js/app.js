9e1c70a01be4277fd005a8f3182e02cdc70bf9ec
Jimi <jimijenneskens@gmail.com>
2020-10-27 13:09:42 -0600

    tablebuilt

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+const tbody = d3.select('tbody');
+
+// Create build Table function
+function buildTable(data){ 
+
+    tbody.html("");
+
+    data.forEach((item) => {
+        var newRow= tbody.append('tr');
+        Object.values(item).forEach((val) => {     
+            newRow.append('td').text(val)
+        });
+    });
+};
+
+buildTable(tableData);
