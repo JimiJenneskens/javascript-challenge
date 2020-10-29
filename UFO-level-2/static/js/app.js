4df6291eb8ae72110e0b021fb2bce25198c3d7e5
Jimi <jimijenneskens@gmail.com>
2020-10-29 12:00:36 -0600

    level2finished

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -22,8 +22,12 @@ function buildTable(data){
 // Create function to Filter date
 function handleClick() {
     
-    // Get the date from the filter
+    // Get the values from the filter
     const date = d3.select("#datetime").property("value");
+    const city = d3.select("#city").property("value");
+    const state = d3.select("#state").property("value");
+    const country = d3.select("#country").property("value");
+    const shape = d3.select("#shape").property("value");
 
     // Create new variable of the original data
     var filterData = tableData; 
@@ -31,6 +35,30 @@ function handleClick() {
     if (date) {
         // Create new table with filtered data
         var filterData = filterData.filter(info => info.datetime === date);
+    };
+
+    if (city) {
+        var lcCity= city.toLowerCase();
+        // Create new table with filtered data
+        var filterData = filterData.filter(info => info.city === lcCity);
+    };
+    
+    if (state) {
+        var lcState= state.toLowerCase();
+        // Create new table with filtered data
+        var filterData = filterData.filter(info => info.state === lcState);
+    };
+
+    if (country) {
+        var lcCountry= country.toLowerCase();
+        // Create new table with filtered data
+        var filterData = filterData.filter(info => info.country === lcCountry);
+    };
+    
+    if (shape) {
+        var lcShape= shape.toLowerCase();
+        // Create new table with filtered data
+        var filterData = filterData.filter(info => info.shape === lcShape);
     };
 
     buildTable(filterData)
